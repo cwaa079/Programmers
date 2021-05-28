5a3197a261d7e9c8e53ffc33b3b1ccf1db72c90d
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-05-28 15:22:48 +0900

    Create 조이스틱.py

---
+++ Lever2/조이스틱.py
@@ -0,0 +1,23 @@
+def solution(name):
+    answer = 0
+    change = [min(ord(spell) - ord('A'), ord('Z') - ord(spell) + 1) for spell in name]
+    answer += sum(change)
+
+    idx = 0
+    while True:
+        change[idx] = 0
+        if sum(change) == 0: break
+        left, right = 1, 1
+        while change[idx - left] == 0:
+            left += 1
+        while change[idx + right] == 0:
+            right += 1
+
+        answer += left if left < right else right
+        idx += -left if left < right else right
+
+    return answer
+  
+#입력
+name = 'JEROEN'
+print(solution(name))
